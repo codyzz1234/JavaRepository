1314ddb247974ba4f955dc6e64ab80cc47268811
cody <kenleybeast1@gmail.com>
2022-04-13 22:31:06 +0800

    First Commit

---
+++ src/Application.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Application{
+	public static void main(String args[]){
+		int aValue;
+		Scanner ce = new Scanner(System.in);
+		System.out.print("Input a value: ");
+		aValue = ce.nextInt();
+		System.out.println("The Value you inputted is " + aValue);
+
+
+	}
+}
---
+++ src/Strings.java
@@ -0,0 +1,11 @@
+public class Strings {
+    public static void main(String[] args) {
+        int aNumber = 9;
+        String text = "Hi";
+        String blank = " ";
+        String moreText = "there";
+        System.out.println("Hi" + blank + moreText);
+
+    }
+
+}
